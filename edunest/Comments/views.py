8b68532bb0cd783d4f9db304910b81213adb88a0
richurichu <paulsonrichu@gmail.com>
2024-03-17 12:53:11 +0530

    new commit

--- edunest/Comments/views.py
+++ edunest/Comments/views.py
@@ -13,7 +13,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 
 class CommentViewSet(viewsets.ModelViewSet):
     
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
 
     def get_queryset(self):
